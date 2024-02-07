cbc28da65455f5fac7d7239d96983166ad506a13
quemin2402 <mereyibraim2006@gmail.com>
2024-02-07 15:20:34 +0600

    new project

---
+++ src/main/java/kz/aitu/bakerywebservice/BakeryWebServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.aitu.bakerywebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeryWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakeryWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/aitu/bakerywebservice/BakeryWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.aitu.bakerywebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
